53a3a70ed283150ce81b82f0f29d390ece441d81
jh20232023 <99hunstory@gmail.com>
2023-09-21 14:24:43 +0900

    23-09-21 base commit

---
+++ app/src/main/java/com/hunstory/tpemptyoutworryjh2023/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.hunstory.tpemptyoutworryjh2023.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hunstory.tpemptyoutworryjh2023.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
