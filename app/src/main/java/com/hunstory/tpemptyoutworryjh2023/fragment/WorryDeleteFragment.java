e99525309e6f4e9cab27328484b8f639c209207f
jh20232023 <99hunstory@gmail.com>
2023-10-11 17:46:34 +0900

    23.10.11 StoryListFragment 완성 ( 상단에 설정된 date 로 Adapter 의도한대로 제작) ++ 감정비우기 tab animotion 완성

--- app/src/main/java/com/hunstory/tpemptyoutworryjh2023/fragment/WorryDeleteFragment.java
+++ app/src/main/java/com/hunstory/tpemptyoutworryjh2023/fragment/WorryDeleteFragment.java
@@ -1,5 +1,9 @@
 package com.hunstory.tpemptyoutworryjh2023.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.app.GameManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,16 +13,128 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.hunstory.tpemptyoutworryjh2023.R;
+import com.hunstory.tpemptyoutworryjh2023.databinding.FragmentWorryBinding;
 
 public class WorryDeleteFragment extends Fragment {
+    FragmentWorryBinding binding;
+    ObjectAnimator animator;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_worry,container,false);
+        binding = FragmentWorryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.ivAngry.setOnClickListener(view1 -> clickIVAngry());
+        binding.ivSad.setOnClickListener(view1 -> clickIVSad());
+        binding.ivSick.setOnClickListener(view1 -> clickIVSick());
+        binding.ivLongface.setOnClickListener(view1 -> clickIVLongface());
+        binding.ivDespair.setOnClickListener(view1 -> clickIVDespair());
+
+    }
+    void clickIVAngry(){
+        binding.ivEmoji.setImageResource(R.drawable.angry);
+        binding.ivEmoji.setVisibility(View.VISIBLE);
+        animator= ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",  600f);
+        animator.setDuration(150);
+        animator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",0,-25f);
+                animator1.setDuration(50).start();
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",-25f,0);
+                animator2.setDuration(50).start();
+                binding.ivEmoji.setVisibility(View.GONE);
+                ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",-120f).start();
+            }
+        });
+        animator.start();
+    }
+    void clickIVSad(){
+        binding.ivEmoji.setImageResource(R.drawable.sad);
+        binding.ivEmoji.setVisibility(View.VISIBLE);
+        animator= ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",  600f);
+        animator.setDuration(150);
+        animator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",0,-25f);
+                animator1.setDuration(50).start();
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",-25f,0);
+                animator2.setDuration(50).start();
+                binding.ivEmoji.setVisibility(View.GONE);
+                ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",-120f).start();
+            }
+        });
+                animator.start();
+    }
+    void clickIVSick(){
+        binding.ivEmoji.setImageResource(R.drawable.sosick);
+        binding.ivEmoji.setVisibility(View.VISIBLE);
+        animator= ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",  600f);
+        animator.setDuration(150);
+        animator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",0,-25f);
+                animator1.setDuration(50).start();
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",-25f,0);
+                animator2.setDuration(50).start();
+                binding.ivEmoji.setVisibility(View.GONE);
+                ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",-120f).start();
+            }
+        });
+        animator.start();
+    }
+    void clickIVLongface(){
+        binding.ivEmoji.setImageResource(R.drawable.longface);
+        binding.ivEmoji.setVisibility(View.VISIBLE);
+        animator= ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",  600f);
+        animator.setDuration(150);
+        animator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",0,-25f);
+                animator1.setDuration(50).start();
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",-25f,0);
+                animator2.setDuration(50).start();
+                binding.ivEmoji.setVisibility(View.GONE);
+                ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",-120f).start();
+            }
+        });
+        animator.start();
+
+    }
+    void clickIVDespair(){
+        binding.ivEmoji.setImageResource(R.drawable.despair_face);
+        binding.ivEmoji.setVisibility(View.VISIBLE);
+        animator= ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",  600f);
+        animator.setDuration(150);
+        animator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",0,-25f);
+                animator1.setDuration(50).start();
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(binding.ivImage,"translationX",-25f,0);
+                animator2.setDuration(50).start();
+                binding.ivEmoji.setVisibility(View.GONE);
+                ObjectAnimator.ofFloat(binding.ivEmoji,"translationY",-120f).start();
+            }
+        });
+        animator.start();
+
     }
 }
